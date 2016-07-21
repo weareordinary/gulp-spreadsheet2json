fefa46b63620de8a692c04410264aeae68af7f73
Ali Elderov <elderov@fundsaccess.com>
2016-07-21 17:29:56 +0200

    update v1.0.1 - add headers with col/rows position and null values

--- index.js
+++ index.js
@@ -1,7 +1,50 @@
 'use strict';
-var gutil = require('gulp-util');
-var through = require('through2');
-var XLSX = require('xlsx');
+
+var gutil = require('gulp-util'),
+    through = require('through2'),
+    XLSX = require('xlsx');
+
+var debug = false;
+
+//TODO: add options for: workbook.Sheets[workbook.SheetNames[0]];
+var log = function(msg) {
+    if (debug) {
+        console.log(msg);
+    }
+};
+
+var getHeaders = function(worksheet, headRow, startColumn) {
+    var headers = [];
+    for (var key in worksheet) {
+
+        var cell = worksheet[key],
+            match = /([A-Z]+)(\d+)/.exec(key);
+
+        if (!match) {
+            continue;
+        }
+
+        var row = match[2]; // 1234
+        var col = match[1]; // ABCD
+
+        if (col >= startColumn) {
+            if (row == headRow) {
+                headers.push(cell.v);
+                // console.log(JSON.stringify(headers));
+            }
+        }
+    }
+    return headers;
+};
+
+var createItem = function(headers) {
+    var obj = {};
+    headers.forEach(function(item) {
+        obj[item] = null;
+    });
+    return obj;
+};
+
 
 /**
  * excel filename or workbook to json
@@ -10,50 +53,69 @@ var XLSX = require('xlsx');
  * @param valueRow
  * @returns {{}} json
  */
-var toJson = function (fileName, headRow, valueRow) {
+var toJson = function(fileName, headRow, valueRow, startColumn) {
+    log('---------- toJSON() -------');
     var workbook;
+
     if (typeof fileName === 'string') {
         workbook = XLSX.readFile(fileName);
     } else {
         workbook = fileName;
     }
-    var worksheet = workbook.Sheets[workbook.SheetNames[0]];
-    var namemap = [];
 
-    // json to return
-    var json = {};
-    var curRow = 0;
+    var worksheet = workbook.Sheets[workbook.SheetNames[0]],
+        json = [],
+        namemap = {},
+        curRow = 0,
+        value,
+        row,
+        col,
+        cell,
+        match,
+        headers = getHeaders(worksheet, headRow, startColumn);
+
     for (var key in worksheet) {
         if (worksheet.hasOwnProperty(key)) {
-            var cell = worksheet[key];
-            var match = /([A-Z]+)(\d+)/.exec(key);
+            cell = worksheet[key];
+            match = /([A-Z]+)(\d+)/.exec(key);
+
             if (!match) {
                 continue;
             }
-            var col = match[1]; // ABCD
-            var row = match[2]; // 1234
-            var value = cell.v;
 
-            if (row == headRow) {
-                namemap[col] = value;
-            } else if (row < valueRow) {
-                //continue;
-            } else {
-                if (col == "A") {
-                    json[cell.v] = {};
-                    curRow = cell.v;
+            row = match[2]; // 1234
+            col = match[1]; // ABCD
+
+            // check if we have a new line
+            if (row > curRow) {
+                curRow = row;
+                namemap = createItem(headers);
+            }
+
+            value = cell.v || null;
+
+            if (col >= startColumn) {
+                if (row < valueRow) {
+                    //continue;
+                } else {
+                    log('-- key[' + col + ':' + row + '] => ' + value);
+
+                    var colIndex = col.charCodeAt() - startColumn.charCodeAt();
+                    namemap[headers[colIndex]] = value;
+
+                    json[row - headRow - 1] = JSON.parse(JSON.stringify(namemap));
                 }
-                json[curRow][namemap[col]] = cell.v;
             }
         }
     }
     return json;
 };
 
-
-module.exports = function (options) {
+module.exports = function(options) {
     options = options || {};
-    return through.obj(function (file, enc, cb) {
+    debug = options.trace || false;
+
+    return through.obj(function(file, enc, cb) {
         if (file.isNull()) {
             this.push(file);
             return cb();
@@ -65,16 +127,18 @@ module.exports = function (options) {
         }
 
         var arr = [];
-        for (var i = 0; i < file.contents.length; ++i) arr[i] = String.fromCharCode(file.contents[i]);
+        for (var i = 0; i < file.contents.length; ++i) {
+            arr[i] = String.fromCharCode(file.contents[i]);
+        }
+
         var bString = arr.join("");
 
         /* Call XLSX */
-        var workbook = XLSX.read(bString, {type: "binary"});
+        var workbook = XLSX.read(bString, {type: "binary", sheetStubs: true, cellHTML: true});
         file.contents = new Buffer(JSON.stringify(toJson(workbook, options.headRow || 1, options.valueRowStart || 2, options.startColumn || 'A')));
 
-        if (options.trace) {
-            console.log("convert file :" + file.path);
-        }
+        log("Convert file: " + file.path);
+
 
         this.push(file);
         cb();
